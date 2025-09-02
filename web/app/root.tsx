7aed8550dec0cfeed598e069f4c022f50697bf73
Alckor <alckordev@gmail.com>
2025-09-02 17:32:08 -0500

    feat(dark): :lipstick: dark mode

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-gray-950 text-gray-200">
         {children}
         <ScrollRestoration />
         <Scripts />
